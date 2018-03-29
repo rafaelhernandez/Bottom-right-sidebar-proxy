d122ec01f78376d92a8f451ead0177129965c52b
Rafael Hernandez <rhmnospam@gmail.com>
2018-03-28 22:35:49 -0700

    Add Redis

--- server/main-router.js
+++ server/main-router.js
@@ -3,19 +3,41 @@ const path = require('path');
 const axios = require('axios');
 const Html = require('./Html.js');
 const env = require('./environment.js');
+const redis = require('./redis.js');
+
 const router = express.Router();
 
 router
   .route('/ssr/:id')
   .get((req, res, next) => {
-    axios
-      .get(`http://${env.sidebarHost}:${env.sidebarPort}/api/sidebar/ssr/${req.params.id}`)
-      .then(html => {
-        res.send(Html.render(html.data));
-      })
-      .catch(err => {
-        console.log('Error retrieving the room data SSR rendered', err);
-      });
+    let id = parseInt(req.params.id);
+    let needToCache = false;
+    let key = 'Proxy_' + id;
+    if (id > 0) {
+      redis.get(key)
+        .then(data => {
+          if (data) {
+            return data;
+          } else {
+            needToCache = true;
+            return axios
+              .get(`http://${env.sidebarHost}:${env.sidebarPort}/api/sidebar/ssr/${id}`);
+          }
+        })
+        .then(html => {
+          let body = html;
+          if (needToCache) { 
+            body = Html.render(html.data);
+            Promise.resolve(redis.set(key, body));
+          }
+          res.send(body);
+        })
+        .catch(err => {
+          console.log('Error ', err);
+        });
+    } else {
+      res.sendStatus(404);
+    }
   })
   .options((req, res) => {
     res.sendStatus(200);
